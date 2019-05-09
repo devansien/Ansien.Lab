304fefcb74eb8a4c409d74e47fb30ca479bf5de8
devansien <devansien@gmail.com>
2019-05-10 01:14:08 +1200

    sagemaker example added

--- Program.cs
+++ Program.cs
@@ -1,27 +1,52 @@
-﻿using System;
+﻿using Amazon;
+using Amazon.SageMakerRuntime;
+using Amazon.SageMakerRuntime.Model;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Ansien.Lab
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Media media = new Media();
-            media["Name"] = "Ansien";
-            media.Id = 0;
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-            Console.WriteLine(media.Id);
-            Console.WriteLine(media["Name"]);
-            Console.WriteLine(media.Setting("Name"));
+            //Media media = new Media();
+            //media["Name"] = "Ansien";
+            //media.Id = 0;
 
-            string targetString = "2";
-            int? number = targetString.ParseInt();
+            //Console.WriteLine(media.Id);
+            //Console.WriteLine(media["Name"]);
+            //Console.WriteLine(media.Setting("Name"));
 
-            if (number != null)
-                Console.WriteLine(number);
-            else
-                Console.WriteLine("Failed to parse a number");
+            //string targetString = "2";
+            //int? number = targetString.ParseInt();
 
+            //if (number != null)
+            //    Console.WriteLine(number);
+            //else
+            //    Console.WriteLine("Failed to parse a number");
+
+            string awsAccessKey = "";
+            string awsSecretKey = "";
+
+            byte[] content = Encoding.ASCII.GetBytes("{\"instances\": [{\"data\": \"abandon\"}]}");
+
+            InvokeEndpointRequest request = new InvokeEndpointRequest();
+            request.Accept = "application/json";
+            request.ContentType = "application/json";
+            request.Body = new MemoryStream(content);
+            request.EndpointName = "Seq2SeqEndpoint-2019-05-09-10-10-22";
+
+            AmazonSageMakerRuntimeClient client = new AmazonSageMakerRuntimeClient(awsAccessKey, awsSecretKey, RegionEndpoint.USEast1);
+            InvokeEndpointResponse response = await client.InvokeEndpointAsync(request);
+
+            string result = Encoding.UTF8.GetString(response.Body.ToArray());
+
+            Console.WriteLine(result);
             Console.ReadKey();
         }
     }
